6caf3594f13d2bf336bad65ace36034442a94993
Linuxliu <Linux_ying@qq.com>
2016-05-06 11:29:56 +0800

    add vimrc

--- lamp/php_lib.sh
+++ lamp/php_lib.sh
@@ -1,24 +1,8 @@
 #!/bin/sh
-
+#by linuxliu
+#mail:512331228@qq.com
+#Version 1.0 2016.05.06
 #编译安装PHP所需的支持库：
-#tar zxf pcre-8.38.tar.gz 
-#cd pcre-8.38
-#./configure 
-#make
-#make install
-#cd ..
-
-#tar zxf nginx-1.9.15.tar.gz
-#cd nginx-1.9.15
-#./configure --user=nginx --group=nginx --prefix=/application/nginx-1.9 --with-http_stub_status_module --with-http_ssl_module
-#make && make install
-#cd ..
-#ln -s /usr/local/lib/libpcre.so.1 /usr/lib64/
-
-#nginx安装完成，fastcgi和php-fpm，php扩展未安装，mysql，php未安装
-
-##php扩展安装
-#cd /home/test/tools/
 
 Tools=/home/test/tools
 
